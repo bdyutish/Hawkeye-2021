793e78dbf7f4059b5389885dfffda868fc1077fd
Akhil Kala <akhilkala@pop-os.localdomain>
2021-05-07 22:25:50 +0530

    notes added

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 default() {
     start
 }
+install(){
+    cd api && npm install && cd ../client && npm install --force
+}
 start(){
-    cd docker && sudo docker-compose -f docker-compose.dev.yml up -d
+    cd docker && sudo docker-compose -f docker-compose.dev.yml up
 }
 stop(){
     cd docker && sudo docker-compose -f docker-compose.dev.yml down -v
