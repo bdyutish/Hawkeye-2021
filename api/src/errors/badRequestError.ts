16f620c9dfd941bcc3cc91aac47d60ab2e45d857
Sarthak Jha <sarthakjha2001@gmail.com>
2021-05-10 14:27:50 +0530

    minor change

---
+++ api/src/errors/badRequestError.ts
@@ -0,0 +1,18 @@
+import { CustomErrors } from './custom-error';
+
+export class BadRequestError extends CustomErrors {
+  statusCode: number = 400;
+  constructor(public mes: string) {
+    super(mes);
+    this.mes = mes;
+
+    Object.setPrototypeOf(this, BadRequestError.prototype);
+  }
+  serializeError() {
+    return [
+      {
+        message: this.mes,
+      },
+    ];
+  }
+}
