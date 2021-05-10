3c4a55652eb8f65b82a6a013a5bbe385e9b190c0
Dyutish <bdyutish@gmail.com>
2021-05-10 16:57:04 +0530

    Helper functions

--- api/src/utils/helperFunctions.ts
+++ api/src/utils/helperFunctions.ts
@@ -1,7 +1,59 @@
+import mongoose from 'mongoose';
 import stringSimilarity from 'string-similarity';
+import User from '../models/User';
+import ErrorResponse from './ErrorResponse';
 
 export const compareAnswers = (input: string, answer: string) => {
   const ratio = stringSimilarity.compareTwoStrings(input, answer);
 
   return ratio;
 };
+
+export const unlockRegion = async (userid: mongoose.Schema.Types.ObjectId) => {
+  const user = await User.findById(userid);
+
+  if (!user) {
+    throw new ErrorResponse('User not found', 404);
+  } else {
+    if (user.lastUnlockedIndex != 6) user.lastUnlockedIndex++;
+    await user.save();
+  }
+};
+
+export const checkUserRegionUnlocked = async (
+  userid: mongoose.Schema.Types.ObjectId,
+  regionId: mongoose.Schema.Types.ObjectId
+) => {
+  const user = await User.findById(userid);
+
+  if (!user) {
+    throw new ErrorResponse('User not found', 404);
+  } else {
+    for (let i = 0; i < user.lastUnlockedIndex; i++) {
+      if (user.regions[i].regionid == regionId) return true;
+    }
+  }
+  return false;
+};
+
+export const hangman = (answer: string) => {
+  let splitted = answer.split('');
+  let count = 0;
+
+  while (count < answer.length / 2) {
+    let index = Math.floor(Math.random() * answer.length); //generate new index
+    if (splitted[index] !== '_' && splitted[index] !== ' ') {
+      splitted[index] = '_';
+      count++;
+    }
+  }
+
+  let newstring = splitted.join('');
+
+  return newstring;
+};
+
+export const multiplyRegion = async (
+  userid: mongoose.Schema.Types.ObjectId,
+  regionId: mongoose.Schema.Types.ObjectId
+) => {};
