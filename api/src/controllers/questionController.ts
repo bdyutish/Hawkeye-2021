a5bfac67bff2c5f9a3d917e8686b8f0023ae66a7
Himanshu <mishra00.11himanshu@gmail.com>
2021-05-12 19:01:08 +0530

    modified editquestion and submit action

--- api/src/controllers/questionController.ts
+++ api/src/controllers/questionController.ts
@@ -1,6 +1,7 @@
 import Question from '../models/Question';
 import { NextFunction, Request, Response } from 'express';
 import ErrorResponse from '../utils/ErrorResponse';
+import { questionRouter } from '../routes/questionRoutes';
 
 export const addQuestion = async (
   req: Request,
@@ -23,4 +24,62 @@ export const addQuestion = async (
   } catch (err) {
     return next(new ErrorResponse(err.name, err.code));
   }
+  
 };
+export const editQuestion = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+
+    const question =  await Question.findByIdAndUpdate(req.params.questionid,req.body)
+    
+
+
+  
+
+    res.status(201).send(question);
+  } catch (err) {
+    return next(new ErrorResponse(err.name, err.code));
+  }
+}
+
+export const getQuestionByRegionId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    //@ts-ignore
+    const questions = await Question.find({region: req.params.regionId})
+    
+
+
+  
+
+    res.status(201).send(questions);
+  } catch (err) {
+    return next(new ErrorResponse(err.name, err.code));
+  }
+}
+
+export const submitQuestion = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const question = await Question.findById(req.params.questionid)
+    question?.keywords.forEach(element => {
+      
+    });    
+
+
+  
+
+    res.status(201).send(questions);
+  } catch (err) {
+    return next(new ErrorResponse(err.name, err.code));
+  }
+}
