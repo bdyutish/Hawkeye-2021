b4df407e5066772ba9ad3544414b0cd4e58ce19d
Hawkeye <hawk@iecse.com>
2021-06-15 14:34:41 +0000

    added deploy script

---
+++ deploy/api-deploy.sh
@@ -0,0 +1,4 @@
+sudo docker pull registry.gitlab.com/iecse-manipal/board-20/prometheus-21/hawkeye-2021/hawk-api:latest
+sudo docker-compose -f /root/hawkeye-2021/docker/docker-compose.prod.yml down
+sudo docker-compose -f /root/hawkeye-2021/docker/docker-compose.prod.yml up -d
+
