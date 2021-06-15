55f9cd1dccc37c7a24e26b2c12bab5581520cefd
Sarthak Jha <sarthakjha2001@gmail.com>
2021-06-16 00:18:44 +0530

    updated depl scripts

--- deploy/api-deploy.sh
+++ deploy/api-deploy.sh
@@ -1,4 +1,13 @@
+echo "Pulling latest api image..."
 sudo docker pull registry.gitlab.com/iecse-manipal/board-20/prometheus-21/hawkeye-2021/hawk-api:latest
-sudo docker-compose -f /root/hawkeye-2021/docker/docker-compose.prod.yml down
-sudo docker-compose -f /root/hawkeye-2021/docker/docker-compose.prod.yml up -d
 
+apiState=$(docker ps -f name=Hawk-api | grep -w Hawk-api)
+if [[ ! -z $apiState ]]
+then
+echo "removing Hawk-api container"
+sudo docker rm -f Hawk-api
+fi
+
+echo "restarting cluster"
+# sudo docker-compose -f /root/hawkeye-2021/docker/docker-compose.prod.yml up -d
+sudo docker-compose -f ../docker/docker-compose.prod.yml up -d
--- deploy/client-deploy.sh
+++ deploy/client-deploy.sh
@@ -1,4 +1,14 @@
+echo "Pulling latest client image..."
 sudo docker pull registry.gitlab.com/iecse-manipal/board-20/prometheus-21/hawkeye-2021/hawk-client:latest
-sudo docker-compose -f /root/hawkeye-2021/docker/docker-compose.prod.yml down
-sudo docker-compose -f /root/hawkeye-2021/docker/docker-compose.prod.yml up -d
 
+apiState=$(docker ps -f name=Hawk-client | grep -w Hawk-client)
+if [[ ! -z $apiState ]]
+then
+
+echo "removing Hawk-client container"
+sudo docker rm -f Hawk-client
+
+fi
+
+echo "Restarting client"
+sudo docker-compose -f /root/hawkeye-2021/docker/docker-compose.prod.yml up -d
