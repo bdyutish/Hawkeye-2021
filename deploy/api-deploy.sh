7865cf34e24c973eea260ab55b581fdf3cbb57b6
Sarthak Jha <sarthakjha2001@gmail.com>
2021-06-17 11:54:09 +0530

    s

--- deploy/api-deploy.sh
+++ deploy/api-deploy.sh
@@ -1,12 +1,12 @@
 echo "Pulling latest api image..."
-sudo docker pull registry.gitlab.com/iecse-manipal/board-20/prometheus-21/hawkeye-2021/hawk-api:latest
+docker pull registry.gitlab.com/iecse-manipal/board-20/prometheus-21/hawkeye-2021/hawk-api:latest
 
 apiState=$(docker ps -a -f name=Hawk-api | grep -w Hawk-api)
 if [[ ! -z $apiState ]]
 then
 echo "removing Hawk-api container"
-sudo docker rm -f Hawk-api
+docker rm -f Hawk-api
 fi
 
 echo "restarting cluster"
-sudo docker-compose -f /root/hawkeye-2021/docker/docker-compose.prod.yml up -d
+docker-compose -f /root/hawkeye-2021/docker/docker-compose.prod.yml up -d
