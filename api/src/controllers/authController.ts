aaf7619daf579cecced85a19ce2006a12f2b5c8f
Dyutish <bdyutish@gmail.com>
2021-06-18 17:18:49 +0530

    test

--- api/src/controllers/authController.ts
+++ api/src/controllers/authController.ts
@@ -184,7 +184,7 @@ export const login = async (
         await log.save();
       }
 
-      res.status(200).send('Logged in successfullyq');
+      res.status(200).send('Logged in successfully');
     }
   } catch (err) {
     return next(new ErrorResponse(err.name, err.code));
