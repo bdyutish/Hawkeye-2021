87d8f8e9b229f8781ed236d1b3cb749ff9e58b6d
Dyutish Bandyopadhyay <bdyutish@gmail.com>
2022-07-29 21:59:46 +0530

    Rulebook updated

--- client/src/components/Rulebook.tsx
+++ client/src/components/Rulebook.tsx
@@ -25,11 +25,11 @@ export default function Rulebook({ closeHandler, open }: Props): ReactElement {
           <ol>
             <li>
               This is an online scavenger hunt, it starts at
-              <span> 19/06/21 12:00</span> and ends on
-              <span> 21/6/21 23:59</span>
+              <span> 30/07/22 12:00</span> and ends on
+              <span> 31/07/22 23:59</span>
             </li>
             <li>
-              You will spawn in a <span>random region</span> out the 6 regions.
+              You will spawn in a <span>region</span> out the 4 regions.
               You have to make your way through the regions one by one by
               solving perplexing riddles. Solve all questions to get a chance to
               beat the hawk.
@@ -71,9 +71,7 @@ export default function Rulebook({ closeHandler, open }: Props): ReactElement {
               <span> ineligible</span> for any prizes. Any suspicious behavior
               will be reported to us by the game.
             </li>
-            <li>
-              Powerups must be applied after buying for them to be effective
-            </li>
+            
           </ol>
         </div>
         <div className="pop2">
@@ -86,63 +84,8 @@ export default function Rulebook({ closeHandler, open }: Props): ReactElement {
               you have solved.
             </li>
             <li>
-              To make it interesting, Hawk presents you with
-              <span> 4</span> different types of <span> Powerups</span>, that
-              you can buy using your reputation points. However, due to a
-              <span> limited supply</span>, there are only a few Powerups of
-              each kind you can buy.
-            </li>
-            <li>
               Winners will be decided on the basis of who has the most
               reputation points at the end of the game and who got there first.
-              Therefore, it’s imperative that you buy and use your
-              <span> Powerups wisely</span> as it affects your position on the
-              leaderboard.
-            </li>
-          </ol>
-        </div>
-        <div className="pop3">
-          <h3>PowerUps</h3>
-          <ol>
-            <li>
-              Region Multiplier - <span>500</span> Points <br />
-              Available:
-              <span> 2</span>
-              <br />
-              Hawk gives you an opportunity to score 1.5x times the points that
-              were previously allotted to the questions. This powerup is valid{" "}
-              <span>ONLY </span>
-              for all questions in that particular region. The multiplier starts
-              from the question, at which it is applied. You can use two such
-              powerups together on the same region to increase the multiplier to
-              <span> 1.5*1.5 = 2.25x times.</span>
-            </li>
-            <li>
-              Question Skip - <span>400</span> Points <br />
-              Available:
-              <span> 1</span> <br />
-              Hawk gives you the opportunity to skip a question! Remember that
-              this will not count as an answered question, and points will not
-              be added for it.
-            </li>
-            <li>
-              Flip a coin <span>200</span> Points <br />
-              Available:
-              <span> 2</span> <br />
-              Hawk flips a biased coin to decide if you skip that question. If
-              it lands on “hawk” you skip that question. Remember that this will
-              not count as an answered question, and points will not be added
-              for it if you skip it.
-            </li>
-            <li>
-              Streak <span>300</span> Points <br />
-              Available: <span> 2</span>
-              <br />
-              Once you purchase this hawk gives you a score multiplier of
-              <span> 2x with 3 strikes</span>. Every incorrect attempt is a
-              strike. 3 strikes remove this extra multiplier. On getting a
-              correct answer while this multiplier is active the number of
-              strikes left is <span>reset to 3.</span>
             </li>
           </ol>
         </div>
@@ -151,8 +94,6 @@ export default function Rulebook({ closeHandler, open }: Props): ReactElement {
           <ol>
             <li>Akhil - 9030544185</li>
             <li>Dyutish - 8777218544</li>
-            <li>Ishan - 9430470186</li>
-            <li>Himanshu - 7260954097</li>
             <li>Nishika - 9810517874</li>
             <li>Sarthak - 9810317930</li>
           </ol>
